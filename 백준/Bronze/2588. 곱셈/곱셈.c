9f28bf814a8d74af91d02c3d3b374eedfd072141
thwang <102675847+thwang26@users.noreply.github.com>
2022-12-22 23:34:50 +0900

    [Bronze III] Title: 곱셈, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int A, B;
+    scanf("%d%d", &A, &B);
+    printf("%d\n", (B % 10)* A);
+    printf("%d\n", ((B/10) % 10)*A);
+    printf("%d\n", ((B/100) % 10)*A);
+    printf("%d\n", B*A);
+}
