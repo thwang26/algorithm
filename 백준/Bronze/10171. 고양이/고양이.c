1d3e5320e09ffce28f79f5b738e66c77c5033180
thwang <102675847+thwang26@users.noreply.github.com>
2022-12-22 23:43:20 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char *str = "\\    /\\";
+    char *str1 = " )  ( ')";
+    char *str2 = "(  /  )";
+    char *str3 = " \\(__)|";
+    printf("%s\n", str);
+    printf("%s\n", str1);
+    printf("%s\n", str2);
+    printf("%s\n", str3);
+    return (0);
+}
