b886609795ef43c5f89a17a29e8699ce5fd81cca
thwang <102675847+thwang26@users.noreply.github.com>
2022-12-23 11:54:52 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("%s\n", "         ,r'\"7");
+    printf("%s\n", "r`-_   ,'  ,/");
+    printf("%s\n", " \\. \". L_r'");
+    printf("%s\n", "   `~\\/");
+    printf("%s\n", "      |");
+    printf("%s\n", "      |");
+}
