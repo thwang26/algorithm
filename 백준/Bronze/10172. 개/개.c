bc982286075e148a7b5e4f39648509dd0ff10f76
thwang <102675847+thwang26@users.noreply.github.com>
2022-12-23 11:51:51 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char *str = "|\\_/|";
+    char *str1 = "|q p|   /}";
+    char *str2 = "( 0 )\"\"\"\\";
+    char *str3 = "|\"^\"`    |";
+    char *str4 = "||_/=\\\\__|";
+    printf("%s\n", str);
+    printf("%s\n", str1);
+    printf("%s\n", str2);
+    printf("%s\n", str3);
+    printf("%s\n", str4);
+    return (0);
+}
