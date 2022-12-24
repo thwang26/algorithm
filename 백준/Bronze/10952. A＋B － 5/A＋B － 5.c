7b331d2ae6fe992e709f755afa9bb2488c0e005d
thwang <102675847+thwang26@users.noreply.github.com>
2022-12-24 22:32:51 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int A, B;
+    scanf("%d %d", &A, &B);
+    while(A != 0 && B != 0)
+    {
+        printf("%d\n", A+B);
+        scanf("%d %d", &A, &B);
+    }
+}
