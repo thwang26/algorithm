0646fc2ee9cac29848abfed9f65d87104d594f95
thwang <102675847+thwang26@users.noreply.github.com>
2022-12-24 22:30:24 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int H, M;
+    scanf("%d%d", &H, &M);
+    if(M >= 45)
+        printf("%d %d", H, M-45);
+    if(M < 45)
+    {    
+        if(H == 0)
+        {
+            printf("23 %d", M+15);
+            return 0;
+        }
+        printf("%d %d", H-1, M+15);
+    }
+}
