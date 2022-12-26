d910d8654b81419ec915cfe810f8e27aa990c6b3
thwang <102675847+thwang26@users.noreply.github.com>
2022-12-26 17:08:46 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 204 ms, Memory: 17628 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -5,10 +5,10 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         
         int[] num = new int[30];
-        for(int i = 0 ; i < 28 ; i++){
+        for(int i = 0 ; i < num.length-2 ; i++){
             num[scan.nextInt()-1] = 1;
         }
-        for(int i = 0 ; i < 30 ; i++){
+        for(int i = 0 ; i < num.length ; i++){
             if(num[i] == 1) continue; 
             else System.out.println(i+1);
         }
