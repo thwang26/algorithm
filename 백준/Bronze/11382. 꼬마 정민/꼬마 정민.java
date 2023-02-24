11efc9c3c2e731928fcedacc327ae37e09a4cf7c
thwang <102675847+thwang26@users.noreply.github.com>
2023-02-24 12:30:12 +0900

    [Bronze V] Title: 꼬마 정민, Time: 212 ms, Memory: 17632 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Long a = scan.nextLong();
+        Long b = scan.nextLong();
+        Long c = scan.nextLong();
+
+        System.out.println(a+b+c);
+    }
+}
