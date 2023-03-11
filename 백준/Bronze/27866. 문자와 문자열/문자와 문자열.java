cb72581cabab572dc7eab60549d72cce0604eadd
thwang <102675847+thwang26@users.noreply.github.com>
2023-03-12 03:07:35 +0900

    [Bronze V] Title: 문자와 문자열, Time: 212 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        
+        String str = scan.next();
+        int n = scan.nextInt();
+        System.out.println(str.charAt(n-1));
+        
+    }
+}
