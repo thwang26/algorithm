d10f3c1a32d4c9bfebf7771da17cfdf069f991f0
thwang <102675847+thwang26@users.noreply.github.com>
2023-03-13 11:33:00 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 2336 ms, Memory: 242036 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Constructor;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    static class Sort implements Comparable<Sort> {
+    
+        int str1;
+        int str2;
+    
+        Sort(int str1, int str2){
+            this.str1 = str1;
+            this.str2 = str2;
+        }
+    
+        @Override
+        public int compareTo(Sort o) {
+            if(this.str2 < o.str2){
+                return -1;
+            } else if(this.str2 > o.str2){
+                return 1;
+            } else {
+                return this.str1 - o.str1;
+            }
+        }
+    }
+    
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        
+        int n = Integer.parseInt(scan.nextLine());
+        String[] str = new String[n];
+
+        Sort[] s = new Sort[n];
+        for(int i = 0 ; i < n ; i++){
+            str[i] = scan.nextLine();
+            int str1 = Integer.parseInt(str[i].split(" ")[0]);
+            int str2 = Integer.parseInt(str[i].split(" ")[1]);
+            s[i] = new Sort(str1, str2);
+        }
+        Arrays.sort(s);
+        for(int i = 0 ; i < n ; i++){
+            System.out.println(s[i].str1+" "+s[i].str2);
+        }
+    }
+}
