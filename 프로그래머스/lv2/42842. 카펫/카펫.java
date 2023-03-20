f690a762518940ae1ab6c6ab5644bfd5824ca16e
thwang <102675847+thwang26@users.noreply.github.com>
2023-03-20 23:29:57 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+
+        for(int i = 1 ; i <= brown ; i++){
+            for(int j = 1; j <= yellow ; j++){
+                if(i*j==yellow && (i+2)*(j+2)==brown+yellow){
+                    answer[0]=i+2;
+                    answer[1]=j+2;
+                }
+            }
+        }
+        return answer;
+    }
+}
