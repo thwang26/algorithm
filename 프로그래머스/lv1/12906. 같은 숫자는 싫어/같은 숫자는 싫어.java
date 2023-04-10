b8972c6d0f2135fb748a0d4451e67df600af8a2d
Taewon Hwang <102675847+thwang26@users.noreply.github.com>
2023-04-10 21:41:09 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 48.80 ms, Memory: 121 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,20 @@
+import java.util.Stack;
+
+class Solution {
+    public int[] solution(int []arr) {
+
+        Stack<Integer> stack = new Stack();
+        stack.push(-1);
+
+        for(int num: arr){
+            if(stack.peek() != num) stack.push(num);
+        }
+
+        int[] answer = new int[stack.size()-1];
+
+        for(int i = answer.length-1 ; i >= 0 ; i--){
+            answer[i] = stack.pop();
+        }
+        return answer;
+    }
+}
