500b595cacad9226ab7ba7ffde4ca1270e6f5eec
Taewon Hwang <102675847+thwang26@users.noreply.github.com>
2023-04-18 05:05:23 +0900

    [level 0] Title: 짝수는 싫어요, Time: 1.88 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -2,7 +2,6 @@ import java.util.stream.IntStream;
 
 class Solution {
     public int[] solution(int n) {
-        int[] answer = IntStream.range(1, n+1).filter(x -> x % 2 != 0).toArray();
-        return answer;
+        return IntStream.range(1, n+1).filter(x -> x % 2 != 0).toArray();
     }
 }
