a9d2c06b3828be346bf0bb855b093ba09ab9c3d4
Taewon Hwang <htw1203@gmail.com>
2023-11-05 19:48:26 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.04 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,28 +1,30 @@
 class Solution {
-
-    public int howOne(String str){
-        int cnt = 0;
-        for(int i = 0 ; i < str.length() ; i++){
-            if(str.charAt(i)=='1') cnt++;
-        }
-        return cnt;
-    }
-
     public int solution(int n) {
-        int answer = 0;
-
-        int i = n+1;
-        String index;
-        String number;
-        while(true){
-            index = Integer.toBinaryString(i);
-            number = Integer.toBinaryString(n);
-            if(howOne(index)==howOne(number)){
+        int answer;
+        int i = n + 1;
+        
+        while (true) {
+            String index = Integer.toBinaryString(i);
+            String number = Integer.toBinaryString(n);
+            if (getOneCount(index) == getOneCount(number)) {
                 answer = i;
                 break;
             }
             i++;
         }
+
         return answer;
+    }
+
+    public int getOneCount(String str) {
+        int cnt = 0;
+        
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '1') {
+                cnt++;
+            }
+        }
+
+        return cnt;
     }
 }
