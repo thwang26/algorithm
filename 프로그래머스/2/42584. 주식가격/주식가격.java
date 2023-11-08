7299ed061a3239403e64ea9686416e5e38eba03c
Taewon Hwang <htw1203@gmail.com>
2023-11-08 16:22:43 +0900

    [level 2] Title: 주식가격, Time: 26.28 ms, Memory: 63.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,28 @@
+import java.util.Stack;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        Stack<Integer> stack = new Stack<>();
+        int[] answer = new int[prices.length];
+
+        stack.add(0);
+        for (int i = 1; i < prices.length; i++) {
+            while (!stack.isEmpty()) {
+                if (prices[stack.peek()] > prices[i]) {
+                    answer[stack.peek()] = i - stack.pop();
+                } else {
+                    break;
+                }
+            }
+
+            stack.push(i);
+        }
+
+        int num = prices.length - 1;
+        while (!stack.isEmpty()) {
+            answer[stack.peek()] = num - stack.pop();
+        }
+        
+        return answer;
+    }
+}
