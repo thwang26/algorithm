4f858d73981357d4af7108a8b7e28f06b9031c60
Taewon Hwang <htw1203@gmail.com>
2023-11-08 17:12:22 +0900

    [level 2] Title: 할인 행사, Time: 0.06 ms, Memory: 82.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -0,0 +1,53 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int solution(String[] want, int[] number, String[] discount) {
+        Map<String, Integer> wantMap = new HashMap<>();
+
+        for (int i = 0; i < want.length; i++) {
+            wantMap.put(want[i], number[i]);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            if (wantMap.containsKey(discount[i])) {
+                wantMap.put(discount[i], wantMap.get(discount[i]) - 1);
+            }
+        }
+
+        int startIdx = 0;
+        int endIdx = startIdx + 10 - 1;
+        int count = 0;
+        while (endIdx < discount.length) {
+            if (satisfaction(wantMap)) {
+                count++;
+            }
+
+            if (endIdx + 1 == discount.length) {
+                break;
+            }
+
+            if (wantMap.containsKey(discount[startIdx])) {
+                wantMap.put(discount[startIdx], wantMap.get(discount[startIdx]) + 1);
+            }
+
+            if (wantMap.containsKey(discount[endIdx + 1])) {
+                wantMap.put(discount[endIdx + 1], wantMap.get(discount[endIdx + 1]) - 1);
+            }
+
+            startIdx++;
+            endIdx++;
+        }
+
+        return count;
+    }
+
+    public boolean satisfaction(Map<String, Integer> map) {
+        for (int value: map.values()) {
+            if (value > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
