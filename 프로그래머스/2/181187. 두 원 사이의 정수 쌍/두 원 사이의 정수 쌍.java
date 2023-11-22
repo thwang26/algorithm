2c95d8181793acd0cbba4f09b832df2d7a6d0724
Taewon Hwang <htw1203@gmail.com>
2023-11-22 14:12:22 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 14.26 ms, Memory: 67.1 MB -BaekjoonHub

--- 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
@@ -1,26 +1,24 @@
 class Solution {
     public long solution(int r1, int r2) {
-        int n = r2 + 1;
-
-        return (getBigOne(n, r2) - getSmallOne(n, r1)) * 4;
+        return (getBigOne(r2) - getSmallOne(r1)) * 4;
     }
 
-    public long getBigOne(int n, int r) {
+    public long getBigOne(int r) {
         long count = 0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < r; i++) {
             count += (long)Math.sqrt(Math.pow(r, 2) - Math.pow(i, 2));
         }
 
         return count;
     }
 
-    public long getSmallOne(int n, int r) {
+    public long getSmallOne(int r) {
         long count = 0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < r; i++) {
             double sqrt = Math.sqrt(Math.pow(r, 2) - Math.pow(i, 2));
 
-            count += (long) sqrt;
-            if (sqrt > 0 && sqrt == (double) (int) sqrt) {
+            count += (long)sqrt;
+            if (sqrt > 0 && sqrt == (double)(int)sqrt) {
                 count--;
             }
         }
