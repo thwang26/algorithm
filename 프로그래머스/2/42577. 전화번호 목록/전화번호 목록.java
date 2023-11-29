c8f5751a5c279c82ed6772e72cf5a7e49ce23410
Taewon Hwang <htw1203@gmail.com>
2023-11-29 14:12:48 +0900

    [level 2] Title: 전화번호 목록, Time: 275.38 ms, Memory: 96.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book, String::compareTo);
+
+        String before = "before";
+        for (String phone: phone_book) {
+            if (phone.startsWith(before)) {
+                return false;
+            }
+            before = phone;
+        }
+
+        return true;
+    }
+}
