4d3d5b3a14c740053868d35ea7771e30569ecccc
Taewon Hwang <htw1203@gmail.com>
2023-12-04 14:26:57 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,16 +1,26 @@
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
     public int[] solution(int n, String[] words) {
         int[] answer = {0, 0};
+        Set<String> set = new HashSet<>();
 
-        for(int i = 1 ; i < words.length ; i++){
-            for(int j = 0 ; j < i ; j++){
-                if(words[j].equals(words[i])||words[i-1].charAt(words[i-1].length()-1)!=words[i].charAt(0)){
-                    answer[0] = i%n+1; 
-                    answer[1] = i/n+1;
-                    return answer;
-                }
+        int idx = 1;
+        set.add(words[0]);
+        char beforeLastCharacter = words[0].charAt(words[0].length() - 1);
+        while (idx < words.length) {
+            String word = words[idx];
+            if (set.contains(word) || beforeLastCharacter != word.charAt(0)) {
+                answer[0] = idx % n + 1;
+                answer[1] = idx / n + 1;
+                break;
             }
+            set.add(word);
+            beforeLastCharacter = word.charAt(word.length() - 1);
+            idx++;
         }
+
         return answer;
     }
 }
