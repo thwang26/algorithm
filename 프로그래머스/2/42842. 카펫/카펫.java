14b70570903754ccf5600f0b674573b676a5e896
Taewon Hwang <htw1203@gmail.com>
2023-12-20 19:58:55 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+
+        for(int i = 1 ; i <= brown ; i++){
+            for(int j = 1; j <= yellow ; j++){
+                if(i*j==yellow && (i+2)*(j+2)==brown+yellow){
+                    answer[0]=j+2;
+                    answer[1]=i+2;
+                    return answer;
+                }
+            }
+        }
+        return answer;
+    }
+}
