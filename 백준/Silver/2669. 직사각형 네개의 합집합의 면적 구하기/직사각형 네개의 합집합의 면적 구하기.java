825535d89994a9385234dac62feff83fade0cdd7
Taewon Hwang <htw1203@gmail.com>
2024-01-15 14:22:01 +0900

    [Silver V] Title: 직사각형 네개의 합집합의 면적 구하기, Time: 212 ms, Memory: 18016 KB -BaekjoonHub

--- 백준/Silver/2669. 직사각형 네개의 합집합의 면적 구하기/직사각형 네개의 합집합의 면적 구하기.java
+++ 백준/Silver/2669. 직사각형 네개의 합집합의 면적 구하기/직사각형 네개의 합집합의 면적 구하기.java
@@ -1,33 +1,22 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    static List<int[]> list = new ArrayList<>();
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int xMax = 0;
-        int yMax = 0;
+        int[][] square = new int[101][101];
         for (int i = 0; i < 4; i++) {
             int[] arr = Arrays.stream(sc.nextLine().split(" "))
                     .mapToInt(Integer::parseInt).toArray();
-            xMax = Math.max(xMax, arr[2]);
-            yMax = Math.max(yMax, arr[3]);
-            list.add(arr);
-        }
-
-        int[][] square = new int[yMax + 1][xMax + 1];
-        for (int[] arr : list) {
-            for (int i = arr[1]; i < arr[3]; i++) {
-                for (int j = arr[0]; j < arr[2]; j++) {
-                    square[i][j] = 1;
+            for (int j = arr[1]; j < arr[3]; j++) {
+                for (int k = arr[0]; k < arr[2]; k++) {
+                    square[j][k] = 1;
                 }
             }
         }
-        
+
         System.out.println(Arrays.stream(square).flatMapToInt(Arrays::stream).sum());
     }
 }
