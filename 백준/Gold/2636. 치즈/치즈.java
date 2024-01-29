b4c4c47384c5f97bfc5b3c8bc44d0c1468145ad5
Taewon Hwang <htw1203@gmail.com>
2024-01-29 15:34:58 +0900

    [Gold IV] Title: 치즈, Time: 328 ms, Memory: 24952 KB -BaekjoonHub

---
+++ 백준/Gold/2636. 치즈/치즈.java
@@ -0,0 +1,79 @@
+import java.util.*;
+
+public class Main {
+    static int[][] direction = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    static List<Integer> cheeseCount = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int y = sc.nextInt();
+        int x = sc.nextInt();
+        int[][] cheese = new int[y][x];
+
+        for (int i = 0; i < y; i++) {
+            for (int j = 0; j < x; j++) {
+                cheese[i][j] = sc.nextInt();
+            }
+        }
+
+        int count = 0;
+        while (countCheese(cheese) != 0) {
+            count++;
+            Queue<Air> queue = new LinkedList<>();
+            boolean[][] visited = new boolean[y][x];
+
+            queue.add(new Air(0, 0));
+            visited[0][0] = true;
+            while (!queue.isEmpty()) {
+                Air air = queue.poll();
+
+                for (int[] each : direction) {
+                    int nx = each[0] + air.x;
+                    int ny = each[1] + air.y;
+
+                    if (isIn(nx, ny, x, y) && !visited[ny][nx]) {
+                        if (cheese[ny][nx] == 0) {
+                            visited[ny][nx] = true;
+                            queue.add(new Air(nx, ny));
+                        } else if (cheese[ny][nx] == 1) {
+                            visited[ny][nx] = true;
+                            cheese[ny][nx] = 0;
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.println(count);
+        System.out.println(cheeseCount.get(cheeseCount.size() - 2));
+    }
+
+    private static int countCheese(int[][] cheese) {
+        int count = 0;
+        for (int[] each : cheese) {
+            for (int n : each) {
+                if (n == 1) {
+                    count++;
+                }
+            }
+        }
+
+        cheeseCount.add(count);
+        return count;
+    }
+
+    private static boolean isIn(int x, int y, int n, int m) {
+        return x >= 0 && x < n && y >= 0 && y < m;
+    }
+
+    static class Air {
+        int x;
+        int y;
+
+        public Air(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
