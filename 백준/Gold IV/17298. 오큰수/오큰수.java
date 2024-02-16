bc891a958184a33d337fe36482e4b7fb8df9f430
Taewon Hwang <htw1203@gmail.com>
2024-02-16 15:06:53 +0900

    [Gold IV] Title: 오큰수, Time: 2160 ms, Memory: 321028 KB -BaekjoonHub

---
+++ 백준/Gold IV/17298. 오큰수/오큰수.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+                arr[stack.pop()] = arr[i];
+            }
+
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            arr[stack.pop()] = -1;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int j : arr) {
+            sb.append(j).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
