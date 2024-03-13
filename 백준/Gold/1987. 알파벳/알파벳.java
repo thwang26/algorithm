55a1e530d505e09509f3dd8bc09b18374e6b9900
Taewon Hwang <htw1203@gmail.com>
2024-03-13 15:04:18 +0900

    [Gold IV] Title: 알파벳, Time: 2916 ms, Memory: 302012 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Main {
+    static int[][] direction = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    static String[][] arr;
+    static boolean[] visited = new boolean[26];
+    static int maxCount;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        arr = new String[n][];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.next().split("");
+        }
+
+        visited[arr[0][0].toCharArray()[0] - 'A'] = true;
+        dfs(0, 0, 1);
+        System.out.println(maxCount);
+    }
+
+    static void dfs(int x, int y, int count) {
+        maxCount = Math.max(maxCount, count);
+
+        for (int[] each : direction) {
+            int nx = x + each[0];
+            int ny = y + each[1];
+
+            if (isIn(nx, ny, arr.length, arr[0].length)
+                    && !visited[arr[ny][nx].toCharArray()[0] - 'A']) {
+                visited[arr[ny][nx].toCharArray()[0] - 'A'] = true;
+                dfs(nx, ny, count + 1);
+                visited[arr[ny][nx].toCharArray()[0] - 'A'] = false;
+            }
+        }
+    }
+
+    static boolean isIn(int x, int y, int n, int m) {
+        return x >= 0 && x < m && y >= 0 && y < n;
+    }
+
+}
