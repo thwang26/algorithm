3b9851f6fb1f8144a82b23ec3102f72971335b0f
Taewon Hwang <htw1203@gmail.com>
2024-10-23 15:46:56 +0900

    [Bronze II] Title: 욱제는 효도쟁이야!!, Time: 668 ms, Memory: 73596 KB -BaekjoonHub

---
+++ 백준/Bronze/14487. 욱제는 효도쟁이야！！/욱제는 효도쟁이야！！.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(sc.nextInt());
+        }
+
+        list.sort(null);
+        int sum = IntStream.range(0, list.size() - 1).map(list::get).sum();
+        System.out.println(sum);
+    }
+}
