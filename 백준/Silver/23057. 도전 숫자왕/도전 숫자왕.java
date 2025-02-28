6859294cca93869015a08666570158a7fe3083cd
Taewon Hwang <htw1203@gmail.com>
2025-02-28 15:11:52 +0900

    [Silver II] Title: 도전 숫자왕, Time: 564 ms, Memory: 96976 KB -BaekjoonHub

--- 백준/Silver/23057. 도전 숫자왕/도전 숫자왕.java
+++ 백준/Silver/23057. 도전 숫자왕/도전 숫자왕.java
@@ -1,14 +1,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Main {
     static Set<Integer> set = new HashSet<>();
     static int[] arr;
-    static int count;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,21 +18,18 @@ public class Main {
         arr = Arrays.stream(br.readLine().split(" "))
                 .mapToInt(Integer::parseInt)
                 .toArray();
-        comb(0, 0);
-        System.out.println(Arrays.stream(arr).sum() - set.size());
+        comb();
+        System.out.println(Arrays.stream(arr).sum() - set.size() + 1);
     }
 
-    private static void comb(int idx, int sum) {
-        count++;
-        if (sum != 0) {
-            set.add(sum);
+    private static void comb() {
+        set.add(0);
+        for (int num : arr) {
+            List<Integer> temp = new ArrayList<>();
+            for (int sum : set) {
+                temp.add(sum + num);
+            }
+            set.addAll(temp);
         }
-
-        if (idx >= arr.length) {
-            return;
-        }
-
-        comb(idx + 1, sum + arr[idx]);
-        comb(idx + 1, sum);
     }
 }
