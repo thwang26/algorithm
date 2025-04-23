5beeb35ccf369e4a2145ac265dc4ce2d22d51696
Taewon Hwang <htw1203@gmail.com>
2025-04-23 16:52:08 +0900

    [Silver III] Title: 2×n 타일링, Time: 108 ms, Memory: 14368 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,17 +1,29 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        long[] dp = new long[1001];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        int n = Integer.parseInt(br.readLine());
+
+        bw.write(String.valueOf(makeTile(n)));
+        bw.flush();
+        bw.close();
+    }
+
+    private static int makeTile(int n) {
+        int[] dp = new int[1001];
         dp[1] = 1;
         dp[2] = 2;
         for (int i = 3; i < dp.length; i++) {
             dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
         }
 
-        int n = sc.nextInt();
-        System.out.println(dp[n]);
+        return dp[n];
     }
 }
