88bba5754017f223d887188c028869185bd1cd44
Taewon Hwang <htw1203@gmail.com>
2025-04-23 16:26:49 +0900

    [Bronze I] Title: 이항 계수 1, Time: 108 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -3,41 +3,24 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.lang.reflect.Constructor;
-import java.math.BigInteger;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Queue;
-import java.util.Scanner;
-import java.util.function.IntToLongFunction;
-import java.util.stream.Stream;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Stack;
-
-import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.stream.IntStream;
 
 public class Main {
-    
-    public static int pactorial(int n){
-        int sum = 1;
-        for(int i = 1 ; i <= n ; i++){
-            sum = sum * i;
-        }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        return sum;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        bw.write(String.valueOf(factorial(n) / (factorial(n - k) * factorial(k))));
+        bw.flush();
+        bw.close();
     }
 
-    //백준
-    public static void main(String[] args) throws IOException {
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        int k = scan.nextInt();
-        
-        System.out.println(pactorial(n)/(pactorial(k)*pactorial(n-k)));
-        
+    private static int factorial(int n) {
+        return IntStream.rangeClosed(1, n).reduce(1, (sum, each) -> sum * each);
     }
 }
