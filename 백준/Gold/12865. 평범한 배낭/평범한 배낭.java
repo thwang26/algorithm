f2f251959a2fc0bdecbda6bf0c7a8d395e66bf56
Taewon Hwang <htw1203@gmail.com>
2025-04-24 21:16:14 +0900

    [Gold V] Title: 평범한 배낭, Time: 200 ms, Memory: 19460 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,25 +1,35 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = sc.nextInt();
-        int k = sc.nextInt();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[k + 1];
+        int[] knapsack = new int[k + 1];
         for (int i = 0; i < n; i++) {
-            int w = sc.nextInt();
-            int v = sc.nextInt();
-
-            for (int j = arr.length - 1; j >= 0; j--) {
-                if (w > j) {
+            st = new StringTokenizer(br.readLine());
+            int weight = Integer.parseInt(st.nextToken());
+            int value = Integer.parseInt(st.nextToken());
+            for (int j = knapsack.length - 1; j >= 0; j--) {
+                if (weight > j) {
                     continue;
                 }
-                arr[j] = Math.max(arr[j], arr[j - w] + v);
+                knapsack[j] = Math.max(knapsack[j], knapsack[j - weight] + value);
             }
         }
 
-        System.out.println(arr[k]);
+        bw.write(String.valueOf(knapsack[k]));
+        bw.flush();
+        bw.close();
     }
 }
