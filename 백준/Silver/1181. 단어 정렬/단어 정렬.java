c2d6a0c435b5a46fcb0b9c1fb0cde01e6e5aec0d
Taewon Hwang <htw1203@gmail.com>
2025-04-28 13:14:57 +0900

    [Silver V] Title: 단어 정렬, Time: 336 ms, Memory: 24124 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -3,62 +3,34 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.lang.reflect.Constructor;
-import java.math.BigInteger;
-import java.util.Arrays;
-import java.util.LinkedHashSet;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Main {
-
-    static class Sort implements Comparable<Sort> {
-    
-        String str;
-    
-        Sort(String str){
-            this.str = str;
-        }
-    
-        @Override
-        public int compareTo(Sort o) {
-            if(this.str.length() < o.str.length()){
-                return -1;
-            } else if(this.str.length() > o.str.length()){
-                return 1;
-            } else {
-                for(int i = 0 ; i < str.length() ; i++){
-                    if(this.str.charAt(i) == o.str.charAt(i)) continue;
-                    else return this.str.charAt(i) - o.str.charAt(i);
-                }
-                return 0;
-            }
-        }
-    }
-    
     public static void main(String[] args) throws IOException {
-        Scanner scan = new Scanner(System.in);
-        
-        int n = Integer.parseInt(scan.nextLine());
-        String[] str = new String[n];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        for(int i = 0 ; i < n ; i++){
-            str[i] = scan.nextLine();
+        int n = Integer.parseInt(br.readLine());
+        Set<String> set = new HashSet<>();
+        while (n-- > 0) {
+            set.add(br.readLine());
         }
-        
-        // 배열을 LinkedHashSet으로 변환
-        LinkedHashSet<String> linkedHashSet = 
-        new LinkedHashSet<>(Arrays.asList(str));
-        
-        // LinkedHashSet을 배열로 변환
-        String[] updateStr = linkedHashSet.toArray(new String[0]);
-        
-        Sort[] s = new Sort[updateStr.length];
-        for(int i = 0 ; i < s.length ; i++){
-            s[i] = new Sort(updateStr[i]);
+
+        List<String> list = new ArrayList<>(set);
+        list.sort((o1, o2) -> {
+            if (o1.length() == o2.length()) {
+                return o1.compareTo(o2);
+            }
+            return o1.length() - o2.length();
+        });
+
+        for (String each : list) {
+            bw.write(each + "\n");
         }
-        Arrays.sort(s);
-        for(int i = 0 ; i < s.length ; i++){
-            System.out.println(s[i].str);
-        }
+        bw.flush();
+        bw.close();
     }
 }
