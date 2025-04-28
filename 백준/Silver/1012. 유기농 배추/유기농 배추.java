e7888bd51b420f32c258aa12a42161d7df6f6feb
Taewon Hwang <htw1203@gmail.com>
2025-04-28 13:53:15 +0900

    [Silver II] Title: 유기농 배추, Time: 136 ms, Memory: 16040 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,62 +1,70 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+    static int[][] direction = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
-        int t = scan.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int t = Integer.parseInt(br.readLine());
         while (t-- > 0) {
-            int m = scan.nextInt();
-            int n = scan.nextInt();
-            int k = scan.nextInt();
-            boolean[][] visited = new boolean[n][m];
-
-            while (k-- > 0) {
-                int x = scan.nextInt();
-                int y = scan.nextInt();
-
-                visited[y][x] = true;
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int row = Integer.parseInt(st.nextToken());
+            int col = Integer.parseInt(st.nextToken());
+            int cabbage = Integer.parseInt(st.nextToken());
+            int[][] arr = new int[row][col];
+            while (cabbage-- > 0) {
+                st = new StringTokenizer(br.readLine());
+                int currentRow = Integer.parseInt(st.nextToken());
+                int currentCol = Integer.parseInt(st.nextToken());
+                arr[currentRow][currentCol] = 1;
             }
 
-            find(visited);
-        }
-    }
-
-    public static void find(boolean[][] visited) {
-        int answer = 0;
-        int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-
-        for (int i = 0; i < visited.length; i++) {
-            for (int j = 0; j < visited[0].length; j++) {
-                if (visited[i][j]) {
-                    answer++;
-                    worm(j, i, visited, dirs);
+            boolean[][] visited = new boolean[row][col];
+            int count = 0;
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++) {
+                    if (!visited[i][j] && arr[i][j] == 1) {
+                        visited[i][j] = true;
+                        count++;
+                        find(visited, arr, i, j);
+                    }
                 }
             }
+            bw.write(String.valueOf(count));
+            bw.write("\n");
         }
-        System.out.println(answer);
+
+        bw.flush();
+        bw.close();
     }
 
-    public static void worm(int x, int y, boolean[][] visited, int[][] dirs) {
+    private static void find(boolean[][] visited, int[][] arr, int i, int j) {
         Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{x, y});
-        visited[y][x] = false;
-
+        queue.add(new int[]{i, j});
         while (!queue.isEmpty()) {
-            int[] coo = queue.poll();
-            int curX = coo[0];
-            int curY = coo[1];
+            int[] current = queue.poll();
 
-            for (int[] dir : dirs) {
-                int nx = curX + dir[0];
-                int ny = curY + dir[1];
-
-                if (nx >= 0 && nx < visited[0].length &&
-                        ny >= 0 && ny < visited.length && visited[ny][nx]) {
-                    queue.add(new int[]{nx, ny});
-                    visited[ny][nx] = false;
+            for (int[] each : direction) {
+                int nRow = each[0] + current[0];
+                int nCol = each[1] + current[1];
+                if (isIn(arr.length, arr[0].length, nRow, nCol) && !visited[nRow][nCol] && arr[nRow][nCol] == 1) {
+                    visited[nRow][nCol] = true;
+                    queue.add(new int[]{nRow, nCol});
                 }
             }
         }
+    }
+
+    private static boolean isIn(int row, int col, int i, int j) {
+        return i >= 0 && i < row && j >= 0 && j < col;
     }
 }
