00869fe6abb3b4fd3ea3fcf9a664e6a9d5b2aa1d
Taewon Hwang <htw1203@gmail.com>
2025-04-29 16:23:35 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 46.12 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/3/12904. 가장 긴 팰린드롬/README.md
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/README.md
@@ -1,10 +1,10 @@
 # [level 3] 가장 긴 팰린드롬 - 12904 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12904?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12904) 
 
 ### 성능 요약
 
-메모리: 88.7 MB, 시간: 76.70 ms
+메모리: 88.8 MB, 시간: 46.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 29일 16:09:14
+2025년 04월 29일 16:23:31
 
 ### 문제 설명
 
--- 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
@@ -1,28 +1,26 @@
-import java.util.*;
-
 class Solution
 {
     public int solution(String s) {
         int[][] dp = new int[s.length()][s.length()];
-        for (int i = s.length() - 1; i >= 0; i--) {
-            for (int j = 0; j < s.length(); j++) {
-                // 시작을 i, 끝을 j 로 지정
-                if (i > j || s.charAt(i) != s.charAt(j)) {
+        int n = s.length();
+        int max = 1;
+        for (int start = n - 1; start >= 0; start--) {
+            for (int end = start; end < n; end++) {
+                if (s.charAt(start) != s.charAt(end)) {
                     continue;
-                } // 시작보다 끝이 작거나, 시작 문자와 끝 문자가 다르면 팰린드롬 판별 x
+                }
 
-                if (i == j || i + 1 == j) {
-                    dp[i][j] = j - i + 1;
-                    continue;
-                } // 한칸 or 두칸 팰린드롬
-
-                if (s.charAt(i + 1) == s.charAt(j - 1) && dp[i + 1][j - 1] != 0) {
-                    dp[i][j] = dp[i + 1][j - 1] + 2;
-                } // 세 칸 이상 팰린드롬
-                // 한칸 안쪽만 팰린드롬임이 확실하면 i ~ j도 팰린드롬임
+                if (end - start < 2) {
+                    dp[start][end] = end - start + 1;
+                } else if (dp[start + 1][end - 1] > 0) {
+                    dp[start][end] = dp[start + 1][end - 1] + 2;
+                }
+                
+                max = Math.max(max, dp[start][end]);
             }
+            
         }
 
-        return Arrays.stream(dp).flatMapToInt(Arrays::stream).max().getAsInt();
+        return max;
     }
 }
