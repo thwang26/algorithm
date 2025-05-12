fba17e3b31744c43426c097d95a14d42e3771f74
Taewon Hwang <htw1203@gmail.com>
2025-05-12 17:52:00 +0900

    Time: 6 ms (26.82%), Space: 44.6 MB (50.32%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -0,0 +1,28 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) {
+            return null;
+        }
+
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o.val));
+
+        for (ListNode each : lists) {
+            while (each != null) {
+                pq.add(new ListNode(each.val));
+                each = each.next;
+            }
+        }
+
+        ListNode head = new ListNode(0);
+        ListNode tail = head;
+        while (!pq.isEmpty()) {
+            ListNode current = pq.poll();
+            tail.next = current;
+            tail = tail.next;
+        }
+        return head.next;
+    }
+}
