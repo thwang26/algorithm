e05f59c5179931f9a1059d01cefed540ad469535
Taewon Hwang <htw1203@gmail.com>
2025-05-12 18:10:40 +0900

    Time: 1 ms (82.17%), Space: 42 MB (45.22%) - LeetHub

---
+++ LeetCode/Easy/1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < word1.length() || i < word2.length(); i++) {
+            if (word1.length() > i) {
+                sb.append(word1.charAt(i));
+            }
+            if (word2.length() > i) {
+                sb.append(word2.charAt(i));
+            }
+        }
+
+        return sb.toString();
+        
+    }
+}
