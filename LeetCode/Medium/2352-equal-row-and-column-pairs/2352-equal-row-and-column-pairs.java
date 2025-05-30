9b3c032f30a6e2ed7ab500fa59ad7ba8c7275fb2
Taewon Hwang <htw1203@gmail.com>
2025-05-30 11:45:36 +0900

    Time: 38 ms (31.79%), Space: 51.6 MB (41.72%) - LeetHub

--- LeetCode/Medium/2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
+++ LeetCode/Medium/2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
@@ -1,27 +1,25 @@
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class Solution {
     public int equalPairs(int[][] grid) {
-        Map<List<Integer>, Integer> map = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
         for (int[] each : grid) {
-            List<Integer> list = Arrays.stream(each)
-                    .boxed()
-                    .collect(Collectors.toList());
-            map.put(list, map.getOrDefault(list, 0) + 1);
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < grid.length; j++) {
+                sb.append(each[j]).append(".");
+            }
+            map.put(sb.toString(), map.getOrDefault(sb.toString(), 0) + 1);
         }
 
         int count = 0;
-        for (int i = 0; i < grid[0].length; i++) {
-            final int j = i;
-            List<Integer> list = Arrays.stream(grid)
-                    .mapToInt(ints -> ints[j])
-                    .boxed().collect(Collectors.toList());
-            if (map.containsKey(list)) {
-                count += map.get(list);
+        for (int j = 0; j < grid.length; j++) {
+            StringBuilder sb = new StringBuilder();
+            for (int[] each : grid) {
+                sb.append(each[j]).append(".");
+            }
+            if (map.containsKey(sb.toString())) {
+                count += map.get(sb.toString());
             }
         }
 
