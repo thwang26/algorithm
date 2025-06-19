a4dd8cf49223c7686f5c781fbd43d6f0acf454bd
Taewon Hwang <htw1203@gmail.com>
2025-06-19 09:45:35 +0900

    [level 2] Title: 순위 검색, Time: 1283.92 ms, Memory: 298 MB -BaekjoonHub

--- 프로그래머스/2/72412. 순위 검색/순위 검색.java
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.java
@@ -2,62 +2,61 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-class Solution {
-    static HashMap<String, List<Integer>> map;
+public class Solution {
+    static Map<String, List<Integer>> map = new HashMap<>();
 
     public int[] solution(String[] info, String[] query) {
         int[] answer = new int[query.length];
-        map = new HashMap<>();
 
         for (String s : info) {
             String[] arr = s.split(" ");
             makeSentence(arr, "", 0);
-        } // info를 통해 검색될 수 있는 쿼리의 모든 경우를 map에 넣음
+        }
 
         for (String key : map.keySet()) {
             Collections.sort(map.get(key));
-        } // list 정렬
+        }
 
-        for (int i = 0; i < query.length; i++) {
+        for (int i = 0; i < answer.length; i++) {
             query[i] = query[i].replaceAll(" and ", "");
-            String[] q = query[i].split(" "); // 쿼리와 점수를 분리
-            answer[i] = map.containsKey(q[0]) ? binarySearch(q[0], Integer.parseInt(q[1])) : 0;
+            String[] q = query[i].split(" ");
+            if (map.containsKey(q[0])) {
+                answer[i] = binarySearch(map.get(q[0]), Integer.parseInt(q[1]));
+            }
+
         }
 
         return answer;
     }
 
-    public int binarySearch(String key, int score) {
-        List<Integer> list = map.get(key);
-        int start = 0;
-        int end = list.size() - 1;
-
-        while (start <= end) {
-            int mid = (start + end) / 2;
-
-            if (list.get(mid) < score) {
-                start = mid + 1;
+    private int binarySearch(List<Integer> list, int n) {
+        int left = 0;
+        int right = list.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (list.get(mid) >= n) {
+                right = mid - 1;
             } else {
-                end = mid - 1;
+                left = mid + 1;
             }
         }
 
-        return list.size() - start;
+        return list.size() - left;
     }
 
-    public void makeSentence(String[] arr, String str, int cnt) {
-        if (cnt == 4) {
+    private void makeSentence(String[] arr, String str, int count) {
+        if (count == 4) {
             if (!map.containsKey(str)) {
                 List<Integer> list = new ArrayList<>();
                 map.put(str, list);
             }
             map.get(str).add(Integer.parseInt(arr[4]));
-
             return;
         }
 
-        makeSentence(arr, str + "-", cnt + 1);
-        makeSentence(arr, str + arr[cnt], cnt + 1);
+        makeSentence(arr, str + "-", count + 1);
+        makeSentence(arr, str + arr[count], count + 1);
     }
 }
